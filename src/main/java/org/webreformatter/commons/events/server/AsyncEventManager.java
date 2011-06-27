f99f15133e939e81d9876a062cc1a5000c608090
mikhail.kotelnikov <none@none>
2011-06-27 11:48:03 +0200

    Asynchronous event manager was added.

---
+++ src/main/java/org/webreformatter/commons/events/server/AsyncEventManager.java
@@ -0,0 +1,166 @@
+package org.webreformatter.commons.events.server;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import org.webreformatter.commons.events.EventListenerRegistry;
+import org.webreformatter.commons.events.EventManager;
+import org.webreformatter.commons.events.IEventListener;
+import org.webreformatter.commons.events.IEventListenerRegistration;
+import org.webreformatter.commons.events.IEventListenerRegistry;
+import org.webreformatter.commons.events.IEventManager;
+
+/**
+ * This implementation of the {@link IEventManager} interface handles events in
+ * separate threads so all operations are performed really asynchronously.
+ * 
+ * @author kotelnikov
+ */
+public class AsyncEventManager implements IEventManager {
+
+    private static ThreadLocal<IEventManager> fLocalEventManager = new ThreadLocal<IEventManager>();
+
+    private Executor fExecutor;
+
+    private IEventListenerRegistry fListenerRegistry;
+
+    public AsyncEventManager() {
+        this(Executors.newCachedThreadPool(), new EventListenerRegistry());
+    }
+
+    /**
+     * @param executor
+     * @param listenerRegistry
+     */
+    public AsyncEventManager(
+        Executor executor,
+        IEventListenerRegistry listenerRegistry) {
+        setExecutor(executor);
+        setListenerRegistry(listenerRegistry);
+    }
+
+    /**
+     * @see org.webreformatter.commons.events.IEventListenerRegistry#addListener(java.lang.Class,
+     *      org.webreformatter.commons.events.IEventListener)
+     */
+    public <E> IEventListenerRegistration addListener(
+        Class<E> eventType,
+        IEventListener<? super E> listener) {
+        return fListenerRegistry.addListener(eventType, listener);
+    }
+
+    /**
+     * @see org.webreformatter.commons.events.IEventManager#fireEvent(java.lang.Object)
+     */
+    public <E> void fireEvent(final E event) {
+        IEventManager manager = getLocalEventManager(false);
+        if (manager != null) {
+            // We are already in a working thread
+            manager.fireEvent(event);
+        } else {
+            fExecutor.execute(new Runnable() {
+                public void run() {
+                    IEventManager manager = getLocalEventManager(true);
+                    manager.fireEvent(event);
+                }
+            });
+        }
+    }
+
+    /**
+     * @see org.webreformatter.commons.events.IEventManager#fireEvent(java.lang.Object,
+     *      org.webreformatter.commons.events.IEventListener)
+     */
+    public <E, L extends IEventListener<? super E>> void fireEvent(
+        final E event,
+        final L listener) {
+        IEventManager manager = getLocalEventManager(false);
+        if (manager != null) {
+            // We are already in a working thread
+            manager.fireEvent(event, listener);
+        } else {
+            fExecutor.execute(new Runnable() {
+                public void run() {
+                    IEventManager manager = getLocalEventManager(true);
+                    manager.fireEvent(event, listener);
+                }
+            });
+        }
+    }
+
+    /**
+     * Returns the event listener regsitry used by all thread-specific event
+     * managers.
+     * 
+     * @return the event listener regsitry used by all thread-specific event
+     *         managers
+     */
+    public IEventListenerRegistry getListenerRegistry() {
+        return fListenerRegistry;
+    }
+
+    /**
+     * @see org.webreformatter.commons.events.IEventListenerRegistry#getListeners(java.lang.Class)
+     */
+    public <E> List<IEventListener<?>> getListeners(Class<E> eventType) {
+        return fListenerRegistry.getListeners(eventType);
+    }
+
+    /**
+     * Returns an event manager associated with the current thread. If there is
+     * no such a manager and the given parameter <code>create</code> is
+     * <code>true</code> then this method creates a new thread-specific event
+     * manager and returns it.
+     * 
+     * @param create if this flag is <code>true</code> and there is no event
+     *        managers associated with the current thread then this method will
+     *        create a new event manager
+     * @return a thread-specific event manager
+     */
+    protected IEventManager getLocalEventManager(boolean create) {
+        IEventManager eventManager = fLocalEventManager.get();
+        if (eventManager == null && create) {
+            eventManager = newEventManager();
+            fLocalEventManager.set(eventManager);
+        }
+        return eventManager;
+    }
+
+    /**
+     * Creates and returns a new thread-local event manager.
+     * 
+     * @return a newly created thread-local event manager
+     */
+    protected EventManager newEventManager() {
+        return new EventManager(this);
+    }
+
+    /**
+     * @see org.webreformatter.commons.events.IEventListenerRegistry#removeListener(java.lang.Class,
+     *      org.webreformatter.commons.events.IEventListener)
+     */
+    public <E> boolean removeListener(
+        Class<E> eventType,
+        IEventListener<? super E> listener) {
+        return fListenerRegistry.removeListener(eventType, listener);
+    }
+
+    /**
+     * Sets a new executor
+     * 
+     * @param executor the executor to set
+     */
+    public void setExecutor(Executor executor) {
+        fExecutor = executor;
+    }
+
+    /**
+     * Sets a new listener registry used by all thread-local event managers
+     * 
+     * @param listenerRegistry the registry to set
+     */
+    public void setListenerRegistry(IEventListenerRegistry listenerRegistry) {
+        fListenerRegistry = listenerRegistry;
+    }
+}
---
+++ src/test/java/org/webreformatter/commons/events/server/AsyncEventManagerTest.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package org.webreformatter.commons.events.server;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.TestCase;
+
+import org.webreformatter.commons.events.IEventListener;
+
+/**
+ * @author kotelnikov
+ */
+public class AsyncEventManagerTest extends TestCase {
+
+    /**
+     * @param name
+     */
+    public AsyncEventManagerTest(String name) {
+        super(name);
+    }
+
+    public void test() throws Exception {
+        test(10000, new Executor() {
+            public void execute(Runnable command) {
+                command.run();
+            }
+        });
+        test(10000, Executors.newCachedThreadPool());
+    }
+
+    /**
+     * This test creates the specified number of events. The registered listener
+     * generates new events for each received events. It is used to check that
+     * the asynchronous event manager properly handles events fired in external
+     * threads as well as in internal (working) threads.
+     * 
+     * @param count
+     * @param executor
+     * @throws Exception
+     */
+    public void test(final int count, final Executor executor) throws Exception {
+        final int totalCycleNumbers = 3;
+        FutureTask<Integer> task = new FutureTask<Integer>(
+            new Callable<Integer>() {
+                public Integer call() throws Exception {
+                    final AsyncEventManager manager = new AsyncEventManager();
+                    manager.setExecutor(executor);
+                    final int[] counter = { 0 };
+                    manager.addListener(
+                        String.class,
+                        new IEventListener<String>() {
+                            public void handleEvent(String event) {
+                                int pos = event.lastIndexOf(".");
+                                int cycle = Integer.parseInt(event
+                                    .substring(pos + 1));
+                                if (cycle < totalCycleNumbers) {
+                                    // Re-fire the event
+                                    event = event.substring(0, pos + 1)
+                                        + (cycle + 1);
+                                    manager.fireEvent(event);
+                                } else {
+                                    counter[0]++;
+                                }
+                            }
+                        });
+                    int cycle = 0;
+                    for (int i = 0; i < count; i++) {
+                        manager.fireEvent("Event-" + i + "." + cycle);
+                    }
+                    while (counter[0] < count) {
+                        Thread.sleep(50);
+                    }
+                    return counter[0];
+                }
+            });
+        Executors.newSingleThreadExecutor().execute(task);
+        Integer result = task.get(1, TimeUnit.SECONDS);
+        assertNotNull(result);
+        assertEquals(count, result.intValue());
+    }
+}
